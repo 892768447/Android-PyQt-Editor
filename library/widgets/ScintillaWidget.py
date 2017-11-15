ac7abb9b5b73b254361d731557e316382c264bc4
Irony <892768447@qq.com>
2017-11-16 00:32:42 +0800

    add some set function

--- library/widgets/ScintillaWidget.py
+++ library/widgets/ScintillaWidget.py
@@ -55,6 +55,127 @@ class ScintillaWidget(QsciScintilla):
 #         self.setAutoCompletionUseSingle(self.AcusNever)
         # 自动提示-单词分隔符,如果设置了lexer则忽略此设置
 #         self.setAutoCompletionWordSeparators(separators)
+        # 自动缩进
+        self.setAutoIndent(True)
+        # 当一行没有其它字符时删除前面的缩进
+        self.setBackspaceUnindents(True)
+        # 括号匹配,参考http://pyqt.sourceforge.net/Docs/QScintilla2/classQsciScintilla.html#ae8277ccb3a2af0ae9a1495d8f8ea0523
+        self.setBraceMatching(self.StrictBraceMatch)  # 严格模式
+        # 设置提示背景颜色
+        self.setCallTipsBackgroundColor(Qt.white)
+        # 设置提示前景颜色
+        self.setCallTipsForegroundColor(Qt.darkGray)
+        # 设置提示高亮颜色
+        self.setCallTipsHighlightColor(Qt.darkBlue)
+        # 设置提示位置,参考http://pyqt.sourceforge.net/Docs/QScintilla2/classQsciScintilla.html#aef97a9061de95a09b57d527f6410881d
+        self.setCallTipsPosition(self.CallTipsBelowText)  # 文字下方
+        # 设置提示样式
+        self.setCallTipsStyle(self.CallTipsNoContext)
+        # 设置插入字符前景颜色
+#         self.setCaretForegroundColor()
+        # 设置插入线背景颜色
+#         self.setCaretLineBackgroundColor()
+        # 设置是否显示插入线
+#         self.setCaretLineVisible(True)
+        # 设置插入字符宽度
+#         self.setCaretWidth(1)
+        # 设置很多颜色
+#         self.setColor()
+        #
+#         self.setContractedFolds(    const QList< int > &     folds)
+        #
+#         self.setEdgeColor()
+        #
+#         self.setEdgeColumn()
+        # 参考http://pyqt.sourceforge.net/Docs/QScintilla2/classQsciScintilla.html#a40b8ec37e068b12d9c83ee497929a00e
+#         self.setEdgeMode()
+        # 设置换行模式?参考http://pyqt.sourceforge.net/Docs/QScintilla2/classQsciScintilla.html#ab4b6b4286a74e173a86de0a7f55241d5
+        # 默认为和系统相关
+#         self.setEolMode()
+        #
+#         self.setEolVisibility(True)
+        # Sets the extra space added to the height of a line above the baseline of the text to extra.
+#         self.setExtraAscent()
+        # Sets the extra space added to the height of a line below the baseline of the text to extra.
+#         self.setExtraDescent()
+        # Set the number of the first visible line to linenr.
+#         self.setFirstVisibleLine()
+        # 设置折叠边距的折叠样式,参考http://pyqt.sourceforge.net/Docs/QScintilla2/classQsciScintilla.html#ae478a896ae32a30e8a375049a3d477e0
+        self.setFolding(self.CircledTreeFoldStyle, 2)  # 圆形正负号
+        # The fold margin may be drawn as a one pixel sized checkerboard
+        # pattern of two colours, fore and back.
+        self.setFoldMarginColors(Qt.red, Qt.yellow)
+        # Sets the background colour of an active hotspot area to col.
+#         self.setHotspotBackgroundColor()
+        # Sets the foreground colour of an active hotspot area to col.
+#         self.setHotspotForegroundColor()
+        # 启用活动热点区域的下划线
+        self.setHotspotUnderline(True)
+        # Enables or disables, according to enable, the wrapping of a hotspot
+        # area to following lines. The default is true.
+        self.setHotspotWrap(True)
+        # Sets the indentation of line line to indentation characters.
+#         self.setIndentation(int line, int indentation)
+        # 缩进指南?Enables or disables, according to enable, this display of
+        # indentation guides.
+        self.setIndentationGuides(True)
+        # 缩进指南背景颜色Set the background colour of indentation guides to col.
+#         self.setIndentationGuidesBackgroundColor()
+        # 缩进指南前景颜色Set the foreground colour of indentation guides to col.
+#         self.setIndentationGuidesForegroundColor()
+        # 不使用tab
+        self.setIndentationsUseTabs(False)
+        # 缩进空格数量
+        self.setIndentationWidth(4)
+        # Enables or disables, according to under, if the indicator indicatorNumber is drawn under or over the text (i.e. in the background or foreground). If indicatorNumber is -1 then the state of all indicators is set.
+#         self.setIndicatorDrawUnder(bool under, int indicatorNumber = -1)
+        # Set the foreground colour of indicator indicatorNumber to col. If indicatorNumber is -1 then the colour of all indicators is set.
+#         self.setIndicatorForegroundColor(QColor col, int indicatorNumber = -1)
+        # Set the foreground colour of indicator indicatorNumber to col when the mouse is over it or the caret moved into it. If indicatorNumber is -1 then the colour of all indicators is set.
+        # self.setIndicatorHoverForegroundColor(col, indicatorNumber =-1)
+        # Set the style of indicator indicatorNumber to style when the mouse is over it or the caret moved into it. If indicatorNumber is -1 then the style of all indicators is set.
+        # 参考http://pyqt.sourceforge.net/Docs/QScintilla2/classQsciScintilla.html#a3333f3a47163153c1bd7db1a362b8974
+#         self.setIndicatorHoverStyle(style, indicatorNumber=-1)
+        # Set the outline colour of indicator indicatorNumber to col. If indicatorNumber is -1 then the colour of all indicators is set. At the moment only the alpha value of the colour has any affect.
+#         self.setIndicatorOutlineColor(col, indicatorNumber=-1)
+        # 页边空白背景色
+#         self.setMarginBackgroundColor(int margin,col)
+        # Enables or disables, according to lnrs, the display of line numbers in margin margin.
+#         self.setMarginLineNumbers(int margin, bool lnrs)
+        # Sets the marker mask of margin margin to mask. Only those markers whose bit is set in the mask are displayed in the margin.
+#         self.setMarginMarkerMask(int margin, int mask)
+        # 设置边距选项
+#         self.setMarginOptions(int options)
+        # Set the number of margins to margins.设置边距
+#         self.setMargins(int margins)
+        # 设置边距背景颜色
+        self.setMarginsBackgroundColor(Qt.gray)
+        #
+#         self.setMarginSensitivity(int margin, bool sens)
+        # 设置边距字体
+#         self.setMarginsFont(font)
+        # 设置边距前景颜色
+        self.setMarginsForegroundColor(Qt.black)
+        # Set the margin text of line line with the text text using the style number style.
+#         self.setMarginText(int line, const QString &text, int style)
+        # Set the margin text of line line with the text text using the style style.
+#         self.setMarginText(int line, const QString &text, const QsciStyle &style)
+        #
+#         self.setMarginText(int line, const QsciStyledText &text)
+        # Set the margin text of line line with the list of styled text text.
+#         self.setMarginText(int line, const QList< QsciStyledText > &text)
+        # 设置边距类型,参考http://pyqt.sourceforge.net/Docs/QScintilla2/classQsciScintilla.html#aedab060e87e0533083ea8f1398302090
+#         self.setMarginType(int maring, MarginType type)
+        # 设置边框宽度
+#         self.setMarginWidth(int margin, int width)
+#         self.setMarginWidth(int margin, QString s)
+        # Set the background colour, including the alpha component, of marker markerNumber to col. If markerNumber is -1 then the colour of all markers is set. The default is white.
+#         self.setMarkerBackgroundColor(Qt.white,-1)
+        # Set the foreground colour of marker markerNumber to col. If
+        # markerNumber is -1 then the colour of all markers is set. The default
+        # is black.
+#         self.setMarkerForegroundColor(Qt.black)
+
         # 设置默认utf8编码
         self.setUtf8(True)
 
